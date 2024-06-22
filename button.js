8a67b400bf5601ec3b3826016826c3c1fc7a303d
Jimi-codes <142645017+Jimi-codes@users.noreply.github.com>
2024-06-22 11:45:11 +0530

    Delete button.js

--- button.js
+++
@@ -1,9 +0,0 @@
-const btnE = document.querySelector(".btn");
-
-btnE.addEventListener("mouseover", (event) => {
-    const x = event.pageX - btnE.offsetLeft;
-    const y = event.pageY - btnE.offsetTop;
-
-    btnE.style.setProperty("--xPos", x + "px")
-    btnE.style.setProperty("--yPos", y + "px")
-});
